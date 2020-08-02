5be53f5f8ae91800df777f2faebab2e86e5ca1ed
MisterQueen <tararikooa@gmail.com>
2020-08-02 19:15:51 +0300

    Updates

---
+++ precache-manifest.19610e01cee50dd03d597cc16540d171.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f2fb2001c07eadfcd0a1eeb80929f52",
+    "url": "/star-wars/index.html"
+  },
+  {
+    "revision": "6efa7e61b892846d051e",
+    "url": "/star-wars/static/css/main.c49f0da6.chunk.css"
+  },
+  {
+    "revision": "b3b2f256d0271b42305c",
+    "url": "/star-wars/static/js/2.4f3f41cb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/star-wars/static/js/2.4f3f41cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6efa7e61b892846d051e",
+    "url": "/star-wars/static/js/main.536186df.chunk.js"
+  },
+  {
+    "revision": "fbb3a5e1526b37212098",
+    "url": "/star-wars/static/js/runtime-main.01aa304a.js"
+  }
+]);
